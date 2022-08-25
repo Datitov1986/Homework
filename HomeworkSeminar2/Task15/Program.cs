c73a47be4bf24107dffbe98583378fe179431539
Dmitri <datitov2009@yandex.ru>
2022-08-25 23:18:25 +0300

    used method in this task

--- HomeworkSeminar2/Task15/Program.cs
+++ HomeworkSeminar2/Task15/Program.cs
@@ -8,9 +8,20 @@
 */
 using System;
 
-Console.Write("Введите число от 1 до 7: ");
+void DayOfWeek(int num)
+{
+if(num < 1 || num > 7)
+    Console.WriteLine("Нет такого дня недели");
+else if(num == 6 || num == 7)
+    Console.WriteLine("Ура! Сегодня выходной!");
+else 
+     Console.WriteLine("Сегодня не выходной, иди трудись");
+}
+
+Console.Write("Введите число от 1 до 7: "); 
 int num = Convert.ToInt32(Console.ReadLine());
-if(num == 6 || num == 7)
+DayOfWeek(num);
+/*if(num == 6 || num == 7)
  {
     Console.WriteLine("Ура! Сегодня выходной!");
  }
@@ -22,3 +33,4 @@ else
 {
     Console.WriteLine("Сегодня не выходной, иди трудись");
 }
+*/
