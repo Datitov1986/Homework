50719577da486ac1e134ad7cb88a2d38b83dc922
Dmitri <datitov2009@yandex.ru>
2022-08-30 00:30:39 +0300

    add Homework Seminar 4 folder

---
+++ HomeworkSeminar4/Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double pow = Math.Pow(A, B);
+Console.WriteLine("Число A в степени B = " + pow);  //Так гораздо проще, чем циклом
