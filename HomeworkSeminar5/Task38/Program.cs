a5bd44796edab61e15293d5ea5d9877541cf5187
Dmitri <datitov2009@yandex.ru>
2022-09-03 23:37:13 +0300

    add Task38 folder

---
+++ HomeworkSeminar5/Task38/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+void FillArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(-20, 50);
+        Console.Write($"{num[i]}, ");
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
