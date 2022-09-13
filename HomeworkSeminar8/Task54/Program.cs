bf1c7fe9992c0d31db988a875c3904c1efd039d3
Dmitri <datitov2009@yandex.ru>
2022-09-13 21:56:58 +0300

    initial commit

---
+++ HomeworkSeminar8/Task54/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] FillArray(int m, int n)
+{
+	int[,] array = new int[m, n];
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			array[i, j] = new Random().Next(0, 10);
+		}
+	}
+	return array;
+}
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write(array[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+void SortArray(int[,] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+       for (int j = i+1; j < array.Length; j++)
+       {
+           if(array[i,j]>array[j,i])
+           {
+               int k=array[i,j];
+               array[i,j]=array[j,i];
+               array[j,i]=k;
+           }
+       }
+   }
+}
+
+Console.WriteLine("Введите колличество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillArray(m, n);
+PrintArray(array);
+SortArray(array);
